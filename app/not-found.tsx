9033d5905cba2d501d92bd972a008eeee8a88a74
Lifguson <lwellsteadgi@gmail.com>
2023-07-15 01:11:48 +0200

    added 404 page;

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col p-16">
+      <div className="flex flex-col text-center">
+        <h1 className="text-8xl self-center">404</h1>
+        <div className="flex flex-col">
+          <p>It seems you are lost...</p>
+          <p>The Chroma Corps Academy is not at this address.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
