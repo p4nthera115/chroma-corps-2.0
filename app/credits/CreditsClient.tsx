38299956c3ba195eed66607ac3225e45d725e8d9
Lifguson <lwellsteadgi@gmail.com>
2023-07-17 12:14:00 +0200

    page template;

--- app/credits/CreditsClient.tsx
+++ app/credits/CreditsClient.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-
 const CreditsClient = () => {
   return (
-    <div
-      className="
+    <section id="credits">
+      <div
+        className="
     inset-0
     flex 
     flex-col
   "
-    >
-      <h1 className="font-cyber font-extrabold text-center text-yellow-400 lg:text-5xl md:text-4xl text-3xl mb-4">
-        CREDITS
-      </h1>
-    </div>
+      >
+        <h1 className="font-cyber font-extrabold text-center text-yellow-400 lg:text-5xl md:text-4xl text-3xl mb-4">
+          CREDITS
+        </h1>
+        <div id="website-credits">
+          <div>
+            <h2>This site was created by @_panthera and @lifguson</h2>
+            <p>github, whatever</p>
+          </div>
+          <div>
+            <h2>special thanks to sinix</h2>
+          </div>
+          <div>
+            <h2>also thanks to sabi</h2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
