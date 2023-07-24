0569440649919c68e705f490b12db9fbb48d97a1
Lifguson <lwellsteadgi@gmail.com>
2023-07-24 12:06:20 +0200

    fixed prompt component;

--- app/components/AssignmentsPage/Prompt.tsx
+++ app/components/AssignmentsPage/Prompt.tsx
@@ -5,11 +5,14 @@ interface PromptProps {
 
 const Prompt: React.FC<PromptProps> = ({ prompt, assignmentNo }) => {
   return (
-    <div className="w-screen mb-8 justify-center items-center flex bg-neutral-900">
-      <h1 className="font-cyber text-7xl flex p-4">
-        Assignment {assignmentNo?.toString()} <br className="p-4" />
+    <div className="w-screen mb-8 justify-center items-center flex flex-row gap-12 bg-neutral-900 px-12 sm:px-12 md:px-8 py-6">
+      <h1 className="font-cyber text-3xl sm:text-3xl md:text-5xl lg:text-6xl">
+        Assignment {assignmentNo?.toString()}:
       </h1>
-      <h1 className="font-cyber text-3xl sm:text-4xl md:text-5xl"> {prompt}</h1>
+      <h1 className="font-cyber text-2xl p-0 sm:pr-4 md:p-0 sm:text-2xl md:text-4xl lg:text-5xl">
+        {" "}
+        {prompt}
+      </h1>
     </div>
   );
 };
