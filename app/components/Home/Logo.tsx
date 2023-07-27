9d63579d1777dc0d5aaaa899b427eba66079b140
p4nthera115 <asdshuja@hotmail.co.uk>
2023-07-27 15:36:21 +0100

    moving light

--- app/components/Home/Logo.tsx
+++ app/components/Home/Logo.tsx
@@ -1,6 +1,8 @@
 import { useGLTF } from "@react-three/drei";
+import { motion } from "framer-motion-3d";
 
 const Logo = () => {
+  // クロマ隊
   const Logo: any = () => {
     const { scene } = useGLTF("/models/Logo.glb");
     return <primitive object={scene} scale={2} />;
