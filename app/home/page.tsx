1f08777bd6009a46d19d0975d8bf3b080e08668e
p4nthera115 <asdshuja@hotmail.co.uk>
2023-08-20 15:08:36 +0100

    please work

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { useState } from "react";
 import HomeClient from "./HomeClient";
+import { LoadingScreen } from "../components/CadetPage/Loading/LoadingScreen";
 
 const HomePage = () => {
-  return <HomeClient />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => setIsLoading(false), 4000);
+
+  return (
+    <div>
+      {isLoading && <LoadingScreen />}
+
+      <HomeClient />
+    </div>
+  );
 };
 
 export default HomePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
-
-import { LoadingScreen } from "./components/CadetPage/Loading/LoadingScreen";
-import { useState } from "react";
 import Home from "./home/page";
 
 export default function page() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  setTimeout(() => setIsLoading(false), 4000);
   return (
     <div className="flex m-0 p-0 max-h-full max-w-full flex-col ">
-      {isLoading && <LoadingScreen />}
-
       <Home />
     </div>
   );
