9e103ca9a3c8bc3a2bfa2f528316afe3b2f72db8
Lifguson <lwellsteadgi@gmail.com>
2023-09-01 18:10:21 +0200

    edited banners;

--- app/components/AssignmentsPage/SmallElimBanner.tsx
+++ app/components/AssignmentsPage/SmallElimBanner.tsx
@@ -26,7 +26,7 @@ const SmallElimBanner: React.FC<EliminatedBannerProps> = ({
         />
       </div>
       <h2 className="absolute z-30 flex font-cyber p-4 left-0 top-0 opacity-75 text-4xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-[6rem] max-h-full max-w-full md:pl-4 ">
-        {eliminated ? "Eliminated:" : "Dropout:"}
+        {eliminated ? "Eliminated" : "Dropout"}
       </h2>
       <h2
         className={`absolute font-cyber p-4 right-0 bottom-0 text-3xl sm:text-3xl md:text-3xl lg:text-6xl xl:text-7xl max-h-full max-w-full z-30`}
--- app/components/AssignmentsPage/StrikeBanner.tsx
+++ app/components/AssignmentsPage/StrikeBanner.tsx
@@ -38,7 +38,7 @@ const StrikeBanner: React.FC<StrikeBannerProps> = ({ strike }) => {
           )}
           <div className="absolute h-full w-full z-20 bg-black/50"></div>
           <h2 className="absolute z-30 flex font-cyber p-4 left-0 top-0 opacity-75 text-5xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-[6rem] max-h-full max-w-full md:pl-4 ">
-            Strike:
+            Strike
           </h2>
           <h2
             className={`absolute font-cyber p-4 right-0 bottom-0 text-3xl sm:text-3xl md:text-3xl lg:text-6xl xl:text-7xl max-h-full max-w-full z-30`}
