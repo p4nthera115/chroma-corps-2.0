4d0be4b9ef3ccb7be1304e26aa8afba4524db647
p4nthera115 <asdshuja@hotmail.co.uk>
2023-09-09 14:51:56 +0100

    image stretching, added sabi spreadsheet link

--- app/components/Cadets/WinnerBanner.tsx
+++ app/components/Cadets/WinnerBanner.tsx
@@ -96,7 +96,7 @@ const WinnerBanner: React.FC<WinnerBannerProps> = ({ winner }) => {
               <img
                 src={winner[0]?.cadet.bannerImg[0]}
                 alt={winner[0]?.cadet?.name}
-                className={`h-full scale-[100%] md:max-w-full min-w-max px-0 md:px-28  sm:scale-[80%] lg:scale-[100%] z-20 ${
+                className={`h-full scale-[100%] md:max-w-full min-w-max px-0 md:px-28  sm:scale-[80%] lg:scale-[100%] z-20 object-contain ${
                   winner[0].cadet.teamColor === winner[1].cadet.teamColor
                     ? // winner[0].cadet.teamColor === winner[2]?.cadet.teamColor
                       null
@@ -128,7 +128,7 @@ const WinnerBanner: React.FC<WinnerBannerProps> = ({ winner }) => {
               <img
                 src={winner[1]?.cadet.bannerImg[0]}
                 alt={winner[1]?.cadet?.name}
-                className={`h-full scale-[100%] md:max-w-full min-w-max px-0 md:px-28 sm:scale-[80%] lg:scale-[100%] z-20 ${
+                className={`h-full scale-[100%] md:max-w-full min-w-max px-0 md:px-28 sm:scale-[80%] lg:scale-[100%] z-20 object-contain ${
                   winner[1].cadet.teamColor === winner[0].cadet.teamColor
                     ? // winner[1].cadet.teamColor === winner[2]?.cadet.teamColor
                       null
@@ -173,7 +173,7 @@ const WinnerBanner: React.FC<WinnerBannerProps> = ({ winner }) => {
               <img
                 src={winner[0]?.cadet.bannerImg[0]}
                 alt={winner[0]?.cadet?.name}
-                className={`h-full scale-[100%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 ${
+                className={`h-full scale-[100%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 object-contain ${
                   winner[0].cadet.teamColor === winner[1].cadet.teamColor &&
                   winner[0].cadet.teamColor === winner[2]?.cadet.teamColor
                     ? null
@@ -205,7 +205,7 @@ const WinnerBanner: React.FC<WinnerBannerProps> = ({ winner }) => {
               <img
                 src={winner[1]?.cadet.bannerImg[0]}
                 alt={winner[1]?.cadet?.name}
-                className={`h-full scale-[100%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 ${
+                className={`h-full scale-[100%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 object-contain ${
                   winner[1].cadet.teamColor === winner[0].cadet.teamColor &&
                   winner[1].cadet.teamColor === winner[2]?.cadet.teamColor
                     ? null
@@ -237,7 +237,7 @@ const WinnerBanner: React.FC<WinnerBannerProps> = ({ winner }) => {
               <img
                 src={winner[2]?.cadet.bannerImg[0]}
                 alt={winner[2]?.cadet.name}
-                className={`h-full scale-[80%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 ${
+                className={`h-full scale-[80%] md:max-w-full sm:scale-[80%] lg:scale-[100%] z-20 object-contain ${
                   winner[2].cadet.teamColor === winner[0].cadet.teamColor &&
                   winner[2].cadet.teamColor === winner[1]?.cadet.teamColor
                     ? null
--- app/loading.tsx
+++ app/loading.tsx
@@ -7,7 +7,7 @@ const loading = () => {
         <img
           src="/images/ChromaCorpsLogoWhite.png"
           alt="logo"
-          className="animate-spin relative flex h-28 w-28 max-w-min max-h-min self-center justify-center items-center "
+          className="animate-spin relative flex h-28 w-28 max-w-min max-h-min self-center justify-center items-center object-contain"
         />
       </div>
       <div className="relative h-screen w-screen blur-xl "></div>
