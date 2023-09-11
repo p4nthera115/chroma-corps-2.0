56c405e8df11a187cf55894df9a94fd1c9987ded
p4nthera115 <asdshuja@hotmail.co.uk>
2023-09-11 15:12:30 +0100

    ass return button

--- app/components/AssignmentsPage/ReturnButton.tsx
+++ app/components/AssignmentsPage/ReturnButton.tsx
@@ -11,15 +11,15 @@ const PrevAssButton: React.FC<ReturnButtonProps> = ({ year }) => {
   const router = useRouter();
 
   return (
-    <div className="absolute top-0 left-0 flex p-4 m-4 ">
+    <div className="absolute top-0 -left-2 flex p-4 m-2 ">
       <button
-        className="md:w-[16rem] h-10 md:border rounded font-cyber hover:text-amber-400 flex flex-row justify-center items-center"
+        className="h-8 gap-4 font-cyber hover:text-amber-400 flex flex-row justify-center items-center"
         onClick={() => router.push(`/cadets/${year}`)}
       >
-        <span className="p-2 flex flex-row md:hidden left-0 absolute text-3xl items-center">
+        <span className="text-3xl">
           <AiOutlineArrowLeft />
         </span>
-        <span className="hidden md:block">Return to {year}</span>
+        <span className="hidden md:flex text-lg">{year}</span>
       </button>
     </div>
   );
