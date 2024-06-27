d3472962cebaa7953b47d6104661e56700833f61
Lifguson <lwellsteadgi@gmail.com>
2024-06-27 14:35:54 -0400

    Refactor: changed <a> tag for next/link <Link> tags

--- app/story/StoryClient.tsx
+++ app/story/StoryClient.tsx
@@ -1,6 +1,13 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const StoryClient = () => {
+  const tabs = ["2X20, 2X21, 2X22, 2X23, 2X24"];
+
+  const pathname = usePathname();
+
   return (
     <div className="inset-0 flex flex-col w-full justify-center">
       <div
@@ -11,41 +18,41 @@ const StoryClient = () => {
           id="Menu-item"
           className="w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
-          <a id="Menu-link-2X20" href="" className="text-3xl font-cyber">
+          <Link id="Menu-link-2X20" href="" className="text-3xl font-cyber">
             2X20
-          </a>
+          </Link>
         </div>
         <div
           id="Menu-item"
           className="w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
-          <a id="Menu-link-2X21" href="" className="text-3xl font-cyber">
+          <Link id="Menu-link-2X21" href="" className="text-3xl font-cyber">
             2X21
-          </a>
+          </Link>
         </div>
         <div
           id="Menu-item"
           className="w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
-          <a id="Menu-link-2X22" href="" className="text-3xl font-cyber">
+          <Link id="Menu-link-2X22" href="" className="text-3xl font-cyber">
             2X22
-          </a>
+          </Link>
         </div>
         <div
           id="Menu-item"
           className="w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
-          <a id="Menu-link-2X23" href="" className="text-3xl font-cyber">
+          <Link id="Menu-link-2X23" href="" className="text-3xl font-cyber">
             2X23
-          </a>
+          </Link>
         </div>
         <div
           id="Menu-item"
           className="w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
-          <a id="Menu-link-2X24" href="" className="text-3xl font-cyber">
+          <Link id="Menu-link-2X24" href="" className="text-3xl font-cyber">
             2X24
-          </a>
+          </Link>
         </div>
       </div>
     </div>
