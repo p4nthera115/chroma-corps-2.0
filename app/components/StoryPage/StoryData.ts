3077d70d79b30552401aa1c24cfebc09245c5e27
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 15:48:03 -0400

    Feat: inserted data for each Year's story

--- app/components/StoryPage/StoryData.ts
+++ app/components/StoryPage/StoryData.ts
@@ -1,9 +1,52 @@
-export const story2X20 = {};
+export const story2X20 = {
+  title: "Semester 1: Matriculation and A Villainous Rival Organization.",
+  body: {
+    para1:
+      "It's the year 2X20.  Hopeful candidates of all sorts are busy getting their applications together to attend one of the most prestigious polymecha academies in existence, Chroma Corps.  Located in a nexus-class pocket universe, Chroma Corps offers the most comprehensive curriculums available, covering everything from interdimensional weaving, to advanced pilot interfacing, to home economics.  Each year, only two percent of applicants from across the known realms are selected to become academy cadets.  Less than one in ten of these cadets will graduate.  Such is life.",
+    para2:
+      "After a semester filled with trips to scenic locales such as candy island and the cinnabun plateaus, things seemed to be going off without a hitch, but little did we know what evil was lurking behind the scenes.  A rival organization named Monochroma had been plotting to sabotage the academy and plunder the vast resources of Chroma Corps.  Luckily, two resourceful cadets managed to discover these villainous plans and put an end to them before any damage could be done.  These cadets' names were Nikittysan and Weiao, and they would soon be announced as full-fledged Chroma Corps graduates.",
+  },
+  imageSrc: "",
+};
 
-export const story2X21 = {};
+export const story2X21 = {
+  title: "Multi-dimensional Chaos",
+  body: {
+    para1:
+      "The year is now 2X21 and the applications are getting weirder and weirder.  A sentient pile of hands... a maybe-not-so-sentient penguin... it seems tales of Chroma Corps have been reaching previously unknown lands.  Administrators Aldoori and Zapata were brought in to manage this new influx.  With these admins, a friendly rivalry was formed, pitting half of the chosen cadets against the other half.  If only that could have been the only drama this year, unfortunately chaos always finds life at Chroma Corps.",
+    para2:
+      "A dimensional soft-serve machine seemed like a welcome addition to the cafeteria, a pleasant reward for the hard-working cadets.  That was until the previous graduates, Weiao and Nikittysan, decided on a dare to see if they could create a swirl of every dimensional flavor possible.  The fabrics of reality folded in on themselves like a fitted sheet, not only did the resulting explosion wipe out most of the academy, but the incident blasted in doppelganger cadets from alternate dimensions of Chroma Corps.  Chaos ensued and many cadets were wiped out, mostly alternate ones, but some locals as well.  When the dimensional dust settled, three cadets remained, Beatrice.. ChedilKM.. and Ozan.  I think they were from this dimension?  Wait, am I from this dimension?  Oh, who really cares, let's just graduate them and move on.",
+  },
+  imageSrc: "",
+};
 
-export const story2X22 = {};
+export const story2X22 = {
+  title: "Construction, Spirit Week, and Biomass Goodness",
+  body: {
+    para1:
+      "It's officially 2X22 and Chroma Corps academy is still in rubble, but that hasn't stopped the flow of applicants, the term must go on.  Contracting agencies have been brought in to help with both the reconstruction and yearly curriculum.  The Becker Group, Sam Who Does Construction, and MDJ Enterprises will each be in charge of one section of cadets.  The days were long and the hours were harsh for the cadets this year as they balanced classes and hard labor.  I tried cheering them up with my homemade stew, but that seemed to only make things worse.  They're all chanting 'Justice for Vevolf!' at me.  Desperate times call for desperate measures.  We're bringing back the ancient custom of Spirit Week to raise morale. ",
+    para2:
+      "Good news!  While the cadets are dancing and having fun, I've developed a chloro-construct capable of using plant-based biomass to finish the remaining construction.  Things will be back to normal, albeit a bit greener, just in time for graduation.  Finally a successful year with cadets Dako and Fat Cat managing to beat out the other cadets and graduate together.  Quite a wholesome end.",
+  },
+  imageSrc: "",
+};
 
-export const story2X23 = {};
+export const story2X23 = {
+  title: "Overgrowth",
+  body: {
+    para1:
+      "Help!  It's 2X23 and the plants seem to have taken on a life of their own.  I came back from vacation and the academy seems to be more plant than building.  They've even grown past the cyber-security firewalls.  Cadets have started showing up left and right.  Have the plants been sending acceptance letters?  There are way too many cadets here and everytime I try to prune them, more keep showing up.  We need to get to the root of this problem.",
+    para2:
+      "The term is almost over and I thought things had stabilized, but the flora keep evolving.  The plants have learned to assimilate the bodies of fallen cadets.  The campus is overrun with plant zombies... only a handful of surviving cadets remain.  I've tasked them with synthesizing a chemical that can put an end to this ceaseless growth.  With the help of cadet Trout fending off waves of leafy undead, cadet TeTed was successful in the chemical bonding procedure.  In the end, it was cadet Double that managed to deploy the biochemical solution in the most explosive way possible.  Once the smoke cleared, it seemed things had finally settled down.  All three cadets would graduate to full Chroma Corps members.  Somehow they even look more innocent and youthful now than they did before the chaos started.",
+  },
+  imageSrc: "",
+};
 
-export const story2X24 = {};
+export const story2X24 = {
+  title: "",
+  body: {
+    para1: "",
+    para2: "",
+  },
+  imageSrc: "",
+};
