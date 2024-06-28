b08d8a787b8eb2fea450c21d83add868b19b9317
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 15:05:53 -0400

    Refactor: removed unused code

--- app/story/StoryClient.tsx
+++ app/story/StoryClient.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
 const StoryClient = () => {
-  const tabs = ["2X20, 2X21, 2X22, 2X23, 2X24"];
-
-  const pathname = usePathname();
-
   return <div className="flex flex-col justify-center mx-auto">text</div>;
 };
 
