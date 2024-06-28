1090a18c6bc06e860092796c70735c4ed4a3935e
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 15:33:16 -0400

    Feat: added functionality to deselect tab and return to main "/story" page if user clicks the already-selected tab

--- app/components/StoryPage/HeaderTabs.tsx
+++ app/components/StoryPage/HeaderTabs.tsx
@@ -12,7 +12,7 @@ export const HeaderTabs = () => {
     <div id="Menu-list" className="flex flex-row w-full justify-center mx-auto">
       <Link
         id="Menu-link-2X20"
-        href="/story/2X20"
+        href={pathname === "/story/2X20" ? "/story" : "/story/2X20"}
         className={`text-3xl font-cyber w-full py-8 px-8 ${
           pathname === "/story/2X20" ? "bg-[#ffe600]" : "bg-neutral-800"
         } border-2 border-neutral-400 ${
@@ -23,7 +23,7 @@ export const HeaderTabs = () => {
       </Link>
       <Link
         id="Menu-link-2X21"
-        href="/story/2X21"
+        href={pathname === "/story/2X21" ? "/story" : "/story/2X21"}
         className={`text-3xl font-cyber w-full py-8 px-8 ${
           pathname === "/story/2X21" ? "bg-[#ffe600]" : "bg-neutral-800"
         } border-2 border-neutral-400 ${
@@ -34,7 +34,7 @@ export const HeaderTabs = () => {
       </Link>
       <Link
         id="Menu-link-2X22"
-        href="/story/2X22"
+        href={pathname === "/story/2X22" ? "/story" : "/story/2X22"}
         className={`text-3xl font-cyber w-full py-8 px-8 ${
           pathname === "/story/2X22" ? "bg-[#ffe600]" : "bg-neutral-800"
         } border-2 border-neutral-400 ${
@@ -45,7 +45,7 @@ export const HeaderTabs = () => {
       </Link>
       <Link
         id="Menu-link-2X23"
-        href="/story/2X23"
+        href={pathname === "/story/2X23" ? "/story" : "/story/2X23"}
         className={`text-3xl font-cyber w-full py-8 px-8 ${
           pathname === "/story/2X23" ? "bg-[#ffe600]" : "bg-neutral-800"
         } border-2 border-neutral-400 ${
