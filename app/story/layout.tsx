8e34949b04212c1c11b35d709ed70bacbd65a9e8
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 15:06:15 -0400

    Refactor: removed unused code and commented out 2X24 Link

--- app/story/layout.tsx
+++ app/story/layout.tsx
@@ -9,7 +9,6 @@ export default function StoryLayout({
   return (
     <div className="flex m-0 p-0 max-h-full max-w-full flex-col gap-10 bg-black">
       <Menu />
-      {/* <div className="inset-0 flex flex-col w-full justify-center"> */}
       <div
         id="Menu-list"
         className="flex flex-row w-full justify-center mx-auto"
@@ -42,13 +41,13 @@ export default function StoryLayout({
         >
           <div>2X23</div>
         </Link>
-        <Link
+        {/* <Link
           id="Menu-link-2X24"
           href="/story/2X24"
           className="text-3xl font-cyber w-full py-8 px-8 bg-neutral-800 border-2 border-neutral-400 text-[#ffe600] text-center hover:cursor-pointer hover:bg-[#ffe600] hover:text-black transition"
         >
           <div>2X24</div>
-        </Link>
+        </Link> */}
       </div>
       {children}
     </div>
