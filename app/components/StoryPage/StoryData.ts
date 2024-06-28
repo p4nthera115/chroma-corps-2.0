4460c5d9be197def92ead807cb2fe31830f672c0
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 15:34:36 -0400

    Refactor: changed data type of each Year from Array to Object

--- app/components/StoryPage/StoryData.ts
+++ app/components/StoryPage/StoryData.ts
@@ -1,9 +1,9 @@
-export const story2X20 = [];
+export const story2X20 = {};
 
-export const story2X21 = [];
+export const story2X21 = {};
 
-export const story2X22 = [];
+export const story2X22 = {};
 
-export const story2X23 = [];
+export const story2X23 = {};
 
-export const story2X24 = [];
+export const story2X24 = {};
