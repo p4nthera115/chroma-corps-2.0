e8d95336c237fcaa5329949d0ffdb20d9600c0b3
Lifguson <lwellsteadgi@gmail.com>
2024-06-29 17:32:29 -0400

    Feat: responsiveness

--- app/story/2X21/2X21Client.tsx
+++ app/story/2X21/2X21Client.tsx
@@ -4,9 +4,9 @@ import { story2X21 } from "@/app/components/StoryPage/StoryData";
 
 const StoryClient = () => {
   return (
-    <div className="flex flex-col justify-center mx-auto text-center h-full w-full">
-      <div className="flex flex-col justify-center mx-auto text-center gap-8 h-full w-1/2">
-        <h2 className="text-4xl font-cyber">{story2X21.title}</h2>
+    <div className="flex flex-col justify-center mx-auto text-center h-full xs:w-1/2 sm:w-3/4 md:w-full md:max-w-full xs:max-w-fit">
+      <div className="flex flex-col justify-center mx-auto text-center gap-8 h-full xs:w-3/4 md:w-1/2">
+        <h2 className="text-4xl font-cyber mx-auto">{story2X21.title}</h2>
         <div className="flex flex-col gap-8 text-lg font-extralight leading-loose">
           <p>{story2X21.body.para1}</p>
           <p>{story2X21.body.para2}</p>
