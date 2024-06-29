95a0a92f76c5463234d0d8abc1f4effdfa5b0802
Lifguson <lwellsteadgi@gmail.com>
2024-06-28 20:48:38 -0400

    Feat: render 2023 story data

--- app/story/2X23/2X23Client.tsx
+++ app/story/2X23/2X23Client.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { story2X23 } from "@/app/components/StoryPage/StoryData";
+
 const StoryClient = () => {
-  return <div className="flex flex-col justify-center mx-auto">2023 text</div>;
+  return (
+    <div className="flex flex-col justify-center mx-auto">
+      <h2 className="text-3xl font-cyber">{story2X23.title}</h2>
+      <div className="flex flex-col gap-4 text-lg">
+        <p>{story2X23.body.para1}</p>
+        <p>{story2X23.body.para2}</p>
+      </div>
+    </div>
+  );
 };
 
 export default StoryClient;
