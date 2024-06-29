14c86c72b97f73a7fc97b3af82e8768005be59c0
Lifguson <lwellsteadgi@gmail.com>
2024-06-29 17:06:04 -0400

    Feat: added image to bottom of text and reformatted some styling

--- app/story/2X20/2X20Client.tsx
+++ app/story/2X20/2X20Client.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { story2X20 } from "@/app/components/StoryPage/StoryData";
+import Image from "next/image";
 
 const StoryClient = () => {
   return (
-    <div className="flex flex-col justify-center mx-auto text-center w-1/2 gap-8 h-full">
-      <h2 className="text-4xl font-cyber">{story2X20.title}</h2>
-      <div className="flex flex-col gap-8 text-lg font-extralight leading-loose">
-        <p>{story2X20.body.para1}</p>
-        <p>{story2X20.body.para2}</p>
+    <div className="flex flex-col justify-center mx-auto text-center h-full w-full">
+      <div className="flex flex-col justify-center mx-auto text-center gap-8 h-full w-1/2">
+        <h2 className="text-4xl font-cyber">{story2X20.title}</h2>
+        <div className="flex flex-col gap-8 text-lg font-extralight leading-loose">
+          <p>{story2X20.body.para1}</p>
+          <p>{story2X20.body.para2}</p>
+        </div>
+      </div>
+      <div className="flex w-full h-full mx-auto py-8">
+        <img
+          src="/images/2020CadetClass.png"
+          alt="2020 Cadet Class"
+          className="max-h-screen max-w-screen mx-auto border-4 border-white"
+        />
       </div>
     </div>
   );
