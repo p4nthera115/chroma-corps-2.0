625149413c24ef28a18fa36562fe6cb0f2a6ac8a
Lifguson <lwellsteadgi@gmail.com>
2024-06-29 17:07:28 -0400

    Feat: added image and improved styling

--- app/story/2X22/2X22Client.tsx
+++ app/story/2X22/2X22Client.tsx
@@ -4,11 +4,20 @@ import { story2X22 } from "@/app/components/StoryPage/StoryData";
 
 const StoryClient = () => {
   return (
-    <div className="flex flex-col justify-center mx-auto text-center w-1/2 gap-8 h-full">
-      <h2 className="text-4xl font-cyber">{story2X22.title}</h2>
-      <div className="flex flex-col gap-8 text-lg font-extralight leading-loose">
-        <p>{story2X22.body.para1}</p>
-        <p>{story2X22.body.para2}</p>
+    <div className="flex flex-col justify-center mx-auto text-center h-full w-full">
+      <div className="flex flex-col justify-center mx-auto text-center gap-8 h-full w-1/2">
+        <h2 className="text-4xl font-cyber">{story2X22.title}</h2>
+        <div className="flex flex-col gap-8 text-lg font-extralight leading-loose">
+          <p>{story2X22.body.para1}</p>
+          <p>{story2X22.body.para2}</p>
+        </div>
+      </div>
+      <div className="flex w-full h-full mx-auto py-8">
+        <img
+          src="/images/2020CadetClass.png"
+          alt="2020 Cadet Class"
+          className="max-h-screen max-w-screen mx-auto border-4 border-white"
+        />
       </div>
     </div>
   );
