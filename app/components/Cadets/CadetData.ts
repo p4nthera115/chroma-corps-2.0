4cdd9e11dd1a2c237a8aa9ad0ffe9faaf41409d5
p4nthera115 <asdshuja@hotmail.co.uk>
2024-07-02 11:14:47 +0200

    updated cadet FDB banner image to default silhouette

--- app/components/Cadets/CadetData.ts
+++ app/components/Cadets/CadetData.ts
@@ -17755,11 +17755,7 @@ export const cadets2020 = [
     strikeBannerPos: "scale-[300%] top-[9rem] h-[20rem]",
     teamColorOpacity: "bg-amber-300/20",
     teamColorHoverOpacity: "hover:bg-amber-300/20",
-    bannerImg: [
-      "https://pub-61ec03a9970d48d19f2731dc689f697b.r2.dev/Cadets/2021/dehaf/dehaf-full.png",
-      "https://pub-61ec03a9970d48d19f2731dc689f697b.r2.dev/Cadets/2021/dehaf/dehaf-full 2.png",
-      "https://pub-61ec03a9970d48d19f2731dc689f697b.r2.dev/Cadets/2021/dehaf/dehaf-full 3.png",
-    ],
+    bannerImg: ["/images/silhouette.png"],
     bannerPos: "absolute h-full scale-[150%] min-w-max",
     cameraPos: "scale-[100%]",
     gradient: "bg-gradient-to-t from-amber-300",
