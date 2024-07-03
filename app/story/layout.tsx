6c6799938c12f8bb67f512b09d36de927e8abdc5
p4nthera115 <asdshuja@hotmail.co.uk>
2024-07-03 16:54:57 +0200

    text-white

--- app/story/layout.tsx
+++ app/story/layout.tsx
@@ -8,7 +8,7 @@ export default function StoryLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex m-0 p-0 max-h-full max-w-full flex-col gap-10 bg-black">
+    <div className="flex m-0 p-0 max-h-full max-w-full flex-col gap-10 bg-black text-white">
       <Menu />
       <HeaderTabs />
       {children}
